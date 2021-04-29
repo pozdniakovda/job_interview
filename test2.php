74dccef1a405af5a02f9ef0c2e9f5b5318919e81
pozdniakovda <82898041+pozdniakovda@users.noreply.github.com>
2021-04-29 19:32:09 +0300

    Update test2.php

--- test2.php
+++ test2.php
@@ -1,6 +1,6 @@
 <?php
 
-//Подключение к Redis
+//Connect to Redis
 function connectToRedis() {
 	$redis = new Redis();
 	$redis->connect('127.0.0.1', 6379);
@@ -8,12 +8,12 @@ function connectToRedis() {
 	return $redis;
 }
 
-//Проверить, существует ли запись с ключом "IP клиента"
+//Check if current IP key exists
 function checkIpExists($conn, $ip) {
 	return $conn->exists($ip);
 }
 
-//Вывести все записи
+//Show all keys
 function showAllKeys($conn) {
 	$allKeys = $conn->keys("*");
 
@@ -23,28 +23,28 @@ function showAllKeys($conn) {
 	}
 }
 
-//Обработать IP
+//Process IP
 function processIp($conn, $ip) {
 	$ipExists = checkIpExists($conn, $ip);
 
-	//Если запись с ключом "IP клиента" существует
+	//If current IP key exists
 	if ($ipExists) {
-		//Увеличить значение на 1
+		//Increment counter by 1
 		$connCount = $conn->get($ip);
 		$conn->set($ip, $connCount + 1);
 	} else {
-		//Иначе добавить запись со значением 1
+		//Else add new key with value of 1
 		$conn->set($ip, 1);
 	}
 }
 
-//Обработать клиент
+//Process client
 function processClient() {
 	$conn = connectToRedis();
 
-	//Обращаюсь напрямую к Apache
+	//If connected directly to Apache
 	$ip = $_SERVER['REMOTE_ADDR'];
-	//В случае Apache+Nginx или иного прокси
+	//If there is Apache+Nginx or other proxy
 	//$ip = $_SERVER['HTTP_X_FORWARDED_FOR']
 
 	processIp($conn, $ip);
@@ -53,7 +53,7 @@ function processClient() {
 	$conn->close();
 }
 
-//Очистить список IP (для наглядности)
+//Clear all key data
 function clearData() {
 	$conn = connectToRedis();
 
@@ -73,5 +73,5 @@ if (isset($_POST['submit'])) {
 ?>
 
 <form action="test2.php" method="POST">
-	<input type="submit" name="submit" value="Очистить">
+	<input type="submit" name="submit" value="Clear">
 </form>
